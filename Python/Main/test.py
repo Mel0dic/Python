08d36a7dad8697de14f0ee91c111dc0ff66b4518
Sectioned <amathisont@gmail.com>
2017-05-16 10:54:02 +0100

    yes

--- Python/Main/test.py
+++ Python/Main/test.py
@@ -1,11 +1,2 @@
-x = 0
-
-while(x < 10):
-    print("Something\n")
-    x = x + 1
-
-for i in range(0, 10):
-    print(i)
-
-if(1 > 0):
-    print("something")
+python = "I love" + "Python"
+print("%s"%python)
