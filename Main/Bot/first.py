812a2399dc48109cb10febbf91e175e064224168
Sectioned <amathisont@gmail.com>
2017-05-22 17:11:59 +0100

    update

--- Main/Bot/first.py
+++ Main/Bot/first.py
@@ -1,9 +1,9 @@
 import tweepy
 
-consumer_key = 'TfD1r7iaAenrw7figv0X0WifD'
-consumer_secret = 'fRUnw1zCPWPoj1BMv3GqN3A5Uw8Xoh1DfWxz2OVAtdEYNqOGOP'
-access_token = 	'866663808441487360-OGyslHHAImLoRHAHPCrWc361gs1ZZjN'
-access_token_secret = 'FsbSUvoFR29EDu8tA6hGzxsQf5c2zQpMU5zPgrSVz0hPN'
+consumer_key = ?
+consumer_secret = ?
+access_token = 	?
+access_token_secret = ?
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
