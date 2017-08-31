1ebe569d95e2e26b71b9ca741018f4d7bb51ded7
Sectioned <Sectioned@users.noreply.github.com>
2017-08-31 11:32:42 +0100

    Book changes
    OK

---
+++ Course/Online/Deep_Learning/Deep_Learning_Book/Chapter1/notes.py
@@ -0,0 +1,9 @@
+ """
+Has it been trained with human supervision
+supervised VS unsupervised VS semisupervised VS reinforcement learning.
+"""
+"""
+Can it learn incrementaly on the fly
+online VS batch learning
+"""
+"""
