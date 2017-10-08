9fdbd450095d1ff944cf27af8fdb02d78580197e
Sectioned <Sectioned@users.noreply.github.com>
2017-10-08 16:29:19 +0100

    t

--- Main/RainMatrix/VControl/ver2.py
+++ Main/RainMatrix/VControl/ver2.py
@@ -5,6 +5,17 @@ os.system('cls')
 row, column = os.get_terminal_size()
 arrs = [[" "] * row for i in range(column)]
 STRINGER = ""
+Numbs = ["0", "1"]
+
+class Dropper():
+	def __init__(self):
+		currentX = 
+		currentY = 
+
+	def getX(self):
+
+	def updat(self):
+
 
 with output(output_type = "list", initial_len = 3, interval = 0) as outputLines:
 
@@ -23,4 +34,14 @@ with output(output_type = "list", initial_len = 3, interval = 0) as outputLines:
 	STRINGER = ""
 	time.sleep(0.1)
 
-#os.system('cls')
+
+
+	for i in range(column):
+		if arrs[0][i] in Numbs:
+			arrs[0][i] == Numbs[random.randint(0, 1)]
+		STRINGER += "".join(arrs[x])
+	outputLines[0] = STRINGER
+	STRINGER = ""
+	time.sleep(0.1)
+
+os.system('cls')
