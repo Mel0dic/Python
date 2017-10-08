a9da38941f9bc5564c4d3cdfaf6b35c965023bb4
Sectioned <Sectioned@users.noreply.github.com>
2017-10-08 14:57:21 +0100

    Ok

--- Main/RainMatrix/VControl/ver2.py
+++ Main/RainMatrix/VControl/ver2.py
@@ -2,25 +2,25 @@ import sys, time, random, os
 from reprint import output
 
 os.system('cls')
-
 row, column = os.get_terminal_size()
-
-refresh = 20
-
-letters = ["0", "1", " "]
-
-arrs = [[" "] * row] * column
-
+arrs = [[" "] * row for i in range(column)]
 STRINGER = ""
 
 with output(output_type = "list", initial_len = 3, interval = 0) as outputLines:
-	for i in range(refresh):
-		for x in range(column):
-			for z in range(row):
-				arrs[x][0] = letters[random.randint(0, 2)]
-			STRINGER += "".join(arrs[x])
-		outputLines[False] = STRINGER
-		STRINGER = ""
-		time.sleep(0.1)
 
-os.system('cls')
+	for x in range(column):
+		for z in range(row):
+			if x == 0:
+				chance = random.randint(0, 100)
+				if chance < 5 and arrs[0][z-1] == " ":
+					arrs[0][z] = "0"
+				elif chance < 10 and arrs[0][z-1] == " ":
+					arrs[0][z] = "1"
+				else:
+					arrs[0][z] = " "
+		STRINGER += "".join(arrs[x])
+	outputLines[0] = STRINGER
+	STRINGER = ""
+	time.sleep(0.1)
+
+#os.system('cls')
