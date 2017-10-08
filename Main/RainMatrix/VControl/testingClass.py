eeaad8a31cdb9e97965b0300d150ce120d42b927
Sectioned <Sectioned@users.noreply.github.com>
2017-10-08 20:20:27 +0100

    FIxIsh

--- Main/RainMatrix/VControl/testingClass.py
+++ Main/RainMatrix/VControl/testingClass.py
@@ -1,10 +1,9 @@
 import sys, time, random, os
 from reprint import output
 
-#os.system('cls' if os.name == 'nt' else 'clear')
+os.system('cls' if os.name == 'nt' else 'clear')
 
 column, row = os.get_terminal_size()
-column -= 1
 
 arrs = [[" "] * column for i in range(row)]
 
@@ -20,12 +19,11 @@ class Symbol:
 	def setRandomSymbol(self):
 		self.value = chr(0x30A0 + random.randint(0, 96))
 		arrs[self.row][self.column]= self.value
-		self.rain()
 
 	def rain(self):
 		self.column += self.speed
 
-symb = Symbol(0, column/2, 1)
+symb = Symbol(0, 0, 1)
 symb.setRandomSymbol()
 
 with output(output_type = "list", initial_len = 3, interval = 0) as outputLines:
@@ -34,6 +32,7 @@ with output(output_type = "list", initial_len = 3, interval = 0) as outputLines:
 			STRINGER += "".join(arrs[i])
 		symb.rain()
 		outputLines[0] = STRINGER
+		STRINGER = ""
 		time.sleep(1)	
 
 #print(STRINGER)
