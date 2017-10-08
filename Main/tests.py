f06517a9a15404bbdc4746ca897722b0368dc6d5
Sectioned <Sectioned@users.noreply.github.com>
2017-10-08 10:14:39 +0100

    ok

--- Main/tests.py
+++ Main/tests.py
@@ -1,19 +1,17 @@
-def median(alist):
-    new = []
-    counter = []
-    for i in range(0, 1000):
-        counter.append(0)
-    for i in range(0, len(alist)):
-        counter[alist[i]] += 1
-    for i in range(0, 1000):
-        if counter[i] > 0:
-            for k in range(0, counter[i]):
-                new.append(i)
-    print(new)
-    if (len(new) % 2) == 0:
-        return (float(new[int((len(new)/2)-1)]) + float(new[int((len(new)/2))])) / 2.0
-    else:
-        return new[int(len(new)/2)] 
-    
-alist = [-1, 2, 6, -5, 4, 2, 9]
-print(median(alist))
+import time,random
+ 
+items = [chr(i) for i in range(0x30a1, 0x30ff + 1)] # katakana
+ 
+ 
+for i in range(1,11): # spaces and numbers
+    items.append(str(i))
+    items.append(" "*i)
+ 
+def rain(row,column):         # rows,columns
+     for i in range(row): #for every row
+             s = ""      #new string
+             for j in range(column): #for every column (or character)
+                     ri = random.randrange(len(items)) #random index
+                     s += items[ri]
+             print(s)
+             time.sleep(0.05) # change this to whatever makes the rain a good speed
