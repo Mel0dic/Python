55ae60d0a42f7337895211868f93b2b22f87aeb5
Ben <amathisont@gmail.com>
2017-10-09 16:15:56 +0100

    Hello

--- Main/RainMatrix/VControl/testingClass.py
+++ Main/RainMatrix/VControl/testingClass.py
@@ -1,32 +1,44 @@
 import sys, time, random, os
 from reprint import output
 
+
+
+
+
 os.system('cls' if os.name == 'nt' else 'clear')
 
 column, row = os.get_terminal_size()
-row += 1
-column += 1
+row -= 1
+
+symbolSize = 1
 
 arrs = [[" "] * column for i in range(row)]
+
+
+
+
 
 def main():
 	global arrs, column, row
 
 	STRINGER = ""
 	Numbs = ["0", "1"]
-	symb = Symbol(0, column/2, 1)
-	symb.setRandomSymbol()
+	stream = Stream()
+	stream.generateSymbols()
 
 	with output(output_type = "list", initial_len = 3, interval = 0) as outputLines:
 		while 1:
 			for i in range(row):
 				STRINGER += "".join(arrs[i])
-			symb.rain()
+			stream.render()
 			outputLines[0] = STRINGER
 			STRINGER = ""
-			time.sleep(1)	
+			time.sleep(0.1)	
 			#print(STRINGER)
 			#print(arrs[int(column/2)][int(row/2)])
+
+
+
 
 
 class Symbol:
@@ -34,14 +46,50 @@ class Symbol:
 		self.row = int(x)
 		self.column= int(y)
 		self.speed = speed
+		self.switchInterval = 0
+		self.value = None
 
 	def setRandomSymbol(self):
-		self.value = chr(0x30A0 + random.randint(0, 96))
-		arrs[self.row][self.column] = self.value
+		if self.switchInterval <= 25:
+			self.value = chr(0x30A0 + random.randint(0, 96))
+			arrs[self.row][self.column] = self.value
+		self.symbolSwitch()
 
 	def rain(self):
-		self.row += self.speed
-		arrs[self.row][self.column] = self.value
+		self.row = 0 if self.row + self.speed >= row else self.row + self.speed
+
+	def symbolSwitch(self):
+		self.switchInterval = random.randint(1, 100)
+
+
+
+
+
+class Stream:
+	def __init__(self):
+		self.symbols = []
+		self.totalSymbols = random.randint(3, 10)
+		self.speed = random.randint(1,3)
+
+	def generateSymbols(self):
+		y = column/2
+		x = 0
+
+		for i in range(0, self.totalSymbols):
+			singleSymbol = Symbol(x, y, self.speed)
+			self.symbols.append(singleSymbol)
+			x -= symbolSize
+
+	def render(self):
+		for eachSymbol in self.symbols:
+			arrs[eachSymbol.row][eachSymbol.column] = " "
+			eachSymbol.rain()
+			arrs[eachSymbol.row][eachSymbol.column] = eachSymbol.value
+			eachSymbol.setRandomSymbol()
+
+
+
+
 
 if __name__ == "__main__":
 	try:
