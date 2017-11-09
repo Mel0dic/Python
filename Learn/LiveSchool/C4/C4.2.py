65efd415d0a081d2d62ab82539f4d0f7f0a7d46e
Sectioned <Sectioned@users.noreply.github.com>
2017-11-09 08:18:52 +0000

    ok

--- Learn/LiveSchool/C4/C4.2.py
+++ Learn/LiveSchool/C4/C4.2.py
@@ -4,43 +4,30 @@ Modularity (OOP) - Object oriented programming. Using objects
 Modularity (Procedural)
 Argument - An argument something that you pass in a function so that you can use that data point in the function.
 Function - Something callable to execute a set of instructions.
-Subroutine
-Variable - Something that holdnds a piece of data and that you can hold.
+Subroutine - A function
+Variable - Something that holdnds a piece of data and that you can edit.
 Data type - Something that is held in a variable
-Constant
+Constant - A variable that once declared cannot be changed.
 Local Variable - A variable accessable in only the function it is declared
-Parameter
-Procedure
-Scope
+Parameter - 
+Procedure - Simmaler to a function but a function will return a value.
+Scope - 
 Global Variable - A variable accessable throughout the program in all functions.
 
 Blocks of Code
-	-
-	-
+	-Function
+	-Subroutine
 Passing Data
-	-
+	-Argument
 	-
 Who can see what
-	-
-	-
+	-Local Variable
+	-Global Variable
 What am I?
-	-
-	-
+	-Modularity (OOP) 
+	-Modularity (Procedural)
+	-Data type
+	-Variable
+	-Constant
 
 '''
-
-def main():
-	#for i in range(1, 11):
-	# 	print(i ** 2)
-	for i in range(2, 101):
-		if isPrime(i):
-			print(i)
-
-def isPrime(query):
-	for i in range(2, query):
-		if query % i == 0:
-			return False
-	return True
-
-if __name__ == "__main__":
-	main()
