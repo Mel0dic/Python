d969e3c22e8c9b87809acd3938a7bb28933e1bdc
Sectioned <Sectioned@users.noreply.github.com>
2017-11-16 12:27:25 +0000

    BChain

--- Main/Blockchain/Tests/hashing.py
+++ Main/Blockchain/Tests/hashing.py
@@ -1,10 +1,35 @@
 import hashlib
 
-list1 = ["a", "b", "c"]
+class Block:
+	def __init__(self, previouseHash, transactions):
+		self.previouseHash = previouseHash
+		self.transactions = transactions
+		self.blockHash = self.hashBlock()
 
-def hash_block(self):
-	sha = hashlib.sha256()
-	sha.update(self)
-	return sha.hexdigest()
+	def hashBlock(self):
+		sha = hashlib.sha256()
+		sha.update(str(self.transactions).encode('utf-8') +
+					str(self.previouseHash).encode('utf-8'))
+		return sha.hexdigest()
 
-hash_block("Hash this")
+
+
+
+
+
+
+
+
+
+
+genesisTransaction = ["satoshi sent 10 bitcoin", "someone sent 10 bitcoin"]
+
+myBlock = Block("0", genesisTransaction)
+
+print(myBlock.blockHash)
+
+block2Transactions = ["everyone lost their bitcoin", "bitcoin"]
+
+myBlock2 = Block(myBlock.blockHash, block2Transactions)
+
+print(myBlock2.blockHash)
