8c5a6d37729c30ddb0a9e101b1d31a070fc1472c
Ben <amathisont@gmail.com>
2017-12-29 21:34:04 +0000

    Ok

--- Main/Practice Projects/GameOfLife/main.py
+++ Main/Practice Projects/GameOfLife/main.py
@@ -1,8 +1,22 @@
 #Game Of Life
 
+import os
+
 #Get's the terminal size
 column, row = os.get_terminal_size()
 
 #Create an array of Spaces the height and width of the terminal
 arrs = [[" "] * column for i in range(row)]
 
+#Define Board as an empty string
+Board = ""
+
+#Transform the array into a single string
+for i in arrs:
+	for charr in i: 
+		Board += charr
+
+#Detect the operating system and clears terminal with apropriate command
+os.system('cls' if os.name == 'nt' else 'clear')
+
+print(Board)
