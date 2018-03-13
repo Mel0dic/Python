157292466dc5d90a07edf1391d5932d0092ae439
Sectioned <Sectioned@users.noreply.github.com>
2018-03-13 15:07:07 +0000

    k

--- RandomProjects/fileDuplicate/renamer.py
+++
@@ -1,9 +0,0 @@
-import os
-
-directory = "C:\\Users\\bengr\\Google Drive\\Curated Dogs"
-os.chdir(directory)
-iterater = 0
-for filename in os.listdir(directory):
-    print(filename)
-    os.renames(filename, str(iterater) + ".jpeg")
-    iterater = iterater + 1
--- RandomProjects/tests.py
+++ RandomProjects/tests.py
@@ -1,10 +1,1 @@
-import urllib2
-url = "http://127.0.0.1:8082"
-
-for i in range(5500, 5601):
-	request = urllib2.request(url, headers=str(i))
-	try:
-		contents = urllib2.urlopen(request).read()
-		print(contents)
-	except:
-		print(url)
+print((1/2) * 0.8 * ((1/(1^2)) + ( 2 * ( (1/(1.8^2)) + (1/(2.6^2)) + (1/(3.4^2)) + (1/(4.2^2)) ) ) + (1/25) ))
